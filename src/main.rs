382ce17fc9842dac2b1438e585635d39e0a81a77
prz23 <panruizheng@zju.edu.cn>
2022-03-31 17:53:27 +0800

    add env_log init()

--- src/main.rs
+++ src/main.rs
@@ -767,5 +767,10 @@ fn error_response(body: String) -> Vec<u8> {
 }
 
 fn main() {
-
+    env_logger::init();
+    println!("start key server fake");
+    let mut config_server = Args::default();
+    config_server.set_port(8000);
+    config_server.set_cert_keys("../test-ca/end.fullchain".to_string(), "../test-ca/end.key".to_string());
+    start_server(config_server, 200);
 }
